5a1a3bc22f8689ac030a3d0c2054a18f23deaadf
Jeniffer Santos <jeniffer.leme@ifsp.edu.br>
2025-10-25 11:38:02 -0300

    Inicialização com a capa

--- app/src/main/java/com/example/guiapocket_bairrox/ui/CapaActivity.kt
+++ app/src/main/java/com/example/guiapocket_bairrox/ui/CapaActivity.kt
@@ -2,18 +2,19 @@ package com.example.guiapocket_bairrox.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.guiapocket_bairrox.R
+import com.example.guiapocket_bairrox.databinding.ActivityCapaBinding
 
 class CapaActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityCapaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_capa)
+        binding = ActivityCapaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnEntrar: Button = findViewById(R.id.btnEntrar)
-        btnEntrar.setOnClickListener {
+        binding.btnEntrar.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
